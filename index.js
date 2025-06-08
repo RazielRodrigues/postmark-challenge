8bc72c287a05f756780c9bde39226bd4ca092dd9
Raziel Rodrigues <razielx3@live.com>
2025-06-08 21:40:53 +0100

    feat: more code

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 const http = require('node:http')
 
 const server = http.createServer({}, async (req, res) => {
-    const devTo = process.env.DEV_TO_TOKEN;
+    try {
 
-    const tokenResponse = await fetch("https://dev.to/api/articles/?username=razielrodrigues", {
-        method: "GET",
-        //headers: {
-        //    "Authorization": "Basic {credentials}",
-        //    "Content-Type": "application/x-www-form-urlencoded",
-        //},
-        //body: new URLSearchParams("grant_type=authorization_code&refresh_token=xxx&client_id=OC-AZLzpxR9SXd1&client_secret=cnvcaIbMrsN8SiRu1DHIXoJnLAq-SETqjDbbhZvgGIG9LJd87a6a3c37"),
-    });
+        const postmark = req.body || {};
 
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    })
-    res.end(JSON.stringify(tokenResponse))
-})
+        console.log('Postmark data:', postmark);
+ 
+/*         const responseDevTo = await fetch("https://dev.to/api/articles", {
+            method: "POST",
+            headers: {
+                "api-key": process.env.DEV_TO_TOKEN,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "article": {
+                    "title": 'TESTE',
+                    "body_markdown": 'TESTE',
+                    "published": false,
+                }
+            }),
+        });
 
-server.listen(3001, "localhost")
-console.log("Listening: http://localhost:3001")
+        const responseData = await responseDevTo.json();  */
+
+        res.writeHead(200, {'Content-Type': 'application/json','Access-Control-Allow-Origin': '*'});
+        res.end(JSON.stringify(postmark));
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
+        res.end(JSON.stringify({ error: error.message }));
+    }
+});
+
+server.listen(3001, "localhost");
+console.log("Listening: http://localhost:3001");
